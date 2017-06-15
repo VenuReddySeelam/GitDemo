88a57012df8fc7443c15cdf4040058faf8c8445a
Admin <admin@Admins-Mac.local>
2017-06-15 18:16:01 +0530

    Initial Commit

---
+++ GitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by Admin on 15/06/2017.
+//  Copyright © 2017 BRNInfoTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitDemo
+//
+//  Created by Admin on 15/06/2017.
+//  Copyright © 2017 BRNInfoTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
